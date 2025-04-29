f3a6aed745dc136c2d56346043e10bbf7751a9a2
Harshali0505 <harshaligaikwad2005@gmail.com>
2025-04-29 16:36:17 +0530

    modified  entrypoint.sh, and configured static files

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Applying database migrations..."
 python manage.py migrate --noinput
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+#python manage.py collectstatic --noinput
 
 echo "Starting Django development server..."
 python manage.py runserver 0.0.0.0:8000
